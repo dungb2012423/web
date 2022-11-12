b081db0e5c63b7deb63c55dd881d9de4662dcd84
dungb2012423 <dungb2012423@student.ctu.edu.vn>
2022-11-12 15:15:04 +0700

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -1,1 +1,22 @@
-asqadtasdadadasdadadasd
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyClI0IetxYyYX15z-JW00sfxU_kdto6108",
+  authDomain: "asdas-a5e4e.firebaseapp.com",
+  databaseURL: "https://asdas-a5e4e-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "asdas-a5e4e",
+  storageBucket: "asdas-a5e4e.appspot.com",
+  messagingSenderId: "257160615063",
+  appId: "1:257160615063:web:949c1191ceaff17c7285d2",
+  measurementId: "G-BLFL94Y2P0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
