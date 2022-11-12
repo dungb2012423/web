e460adb2f1c38b2a65025f74c18f39935aa698c2
dungb2012423 <dungb2012423@student.ctu.edu.vn>
2022-11-12 15:30:02 +0700

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -7,14 +7,14 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyClI0IetxYyYX15z-JW00sfxU_kdto6108",
-  authDomain: "asdas-a5e4e.firebaseapp.com",
-  databaseURL: "https://asdas-a5e4e-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "asdas-a5e4e",
-  storageBucket: "asdas-a5e4e.appspot.com",
-  messagingSenderId: "257160615063",
-  appId: "1:257160615063:web:949c1191ceaff17c7285d2",
-  measurementId: "G-BLFL94Y2P0"
+  apiKey: "AIzaSyBv3c_tubMxjJsvXkMRVF_3YD6AHzo80RA",
+  authDomain: "hehe-18502.firebaseapp.com",
+  databaseURL: "https://hehe-18502-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "hehe-18502",
+  storageBucket: "hehe-18502.appspot.com",
+  messagingSenderId: "838268224933",
+  appId: "1:838268224933:web:26d61e70374581f278944c",
+  measurementId: "G-H8GJHFFV7L"
 };
 
 // Initialize Firebase
